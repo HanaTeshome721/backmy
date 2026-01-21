0e78279469b66a8f12798e8ca1eae8a305522870
HanaTeshome721 <hana2729tm@gmail.com>
2026-01-21 12:13:24 +0300

    feat: updated app.js

--- src/app.js
+++ src/app.js
@@ -21,13 +21,27 @@ app.use(cors({
 
 //import the route
 
-import  healthCheck  from "./routes/healthcheck.routes.js";
-import authRouther from "./routes/auth.routes.js"
+import healthCheck from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import { itemRouter } from "./routes/item.routes.js";
+import requestRouter from "./routes/request.routes.js";
+import complaintRouter from "./routes/complaint.routes.js";
+import notificationRouter from "./routes/notification.routes.js";
+import userRouter from "./routes/user.routes.js";
+import categoryRouter from "./routes/category.routes.js";
+import reportRouter from "./routes/report.routes.js";
 
 
 
-app.use('/api/v1/healthcheck',healthCheck)
-app.use('/api/v1/auth',authRouther)
+app.use("/api/v1/healthcheck", healthCheck);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/items", itemRouter);
+app.use("/api/v1/requests", requestRouter);
+app.use("/api/v1/complaints", complaintRouter);
+app.use("/api/v1/notifications", notificationRouter);
+app.use("/api/v1/admin/users", userRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/reports", reportRouter);
 
 
 app.get('/',(req,res)=>{
